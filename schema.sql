98a4f46a98594024066a3a4d6fd58ef6ba2a80d1
5K1PY <daniel@honza.info>
2022-10-09 08:40:40 +0200

    Named constraints

--- schema.sql
+++ schema.sql
@@ -11,9 +11,9 @@ CREATE TABLE poll_options (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     poll_id TEXT,
     option TEXT NOT NULL,
-    FOREIGN KEY (poll_id)
-        REFERENCES polls (id)
-            ON UPDATE CASCADE
+    CONSTRAINT fk_poll_id
+        FOREIGN KEY (poll_id)
+        REFERENCES polls(id)
             ON DELETE CASCADE
 );
 
@@ -22,8 +22,8 @@ CREATE TABLE poll_data (
     poll_option_id INTEGER,
     user TEXT NOT NULL,
     entry TEXT NOT NULL,
-    FOREIGN KEY (poll_option_id)
-        REFERENCES poll_options (id)
-            ON UPDATE CASCADE
+    CONSTRAINT fk_poll_option_id
+        FOREIGN KEY (poll_option_id)
+        REFERENCES poll_options(id)
             ON DELETE CASCADE
 );
