8e0e4415077949494b82007f214f2f74d01d8d4e
5K1PY <daniel@honza.info>
2022-10-11 21:42:31 +0200

    Bugfix interval mode

--- static/js/poll.js
+++ static/js/poll.js
@@ -11,7 +11,7 @@ $(function() {
     console.log(options);
 
     $this.find('.option-select').on('change', function() {
-        var i = parseInt($(this).attr('id').match(/^options-(\d)/)[1]);
+        var i = parseInt($(this).attr('id').match(/^options-(\d+)$/)[1]);
         if ($this.find('#interval-mode').is(":checked")) {
             console.log(options);
             for (var j=i+1; j < options.length && options[i] === options[j]; j++) {
