603615ea2d864e049b9915267964ab0b65cded7e
5K1PY <daniel@honza.info>
2022-10-11 21:42:52 +0200

    Added options summary

--- poll.py
+++ poll.py
@@ -2,7 +2,7 @@ import calendar
 from datetime import date
 import re
 
-from constants import AVAILABILTY, DEFAULT_AVAILABILITY
+from constants import AVAILABILTY, AVAILABLE, DEFAULT_AVAILABILITY, NOT_PREFERED
 
 class Option:
     def __init__(self, option_id, text):
@@ -39,6 +39,15 @@ class Poll:
         for option_id, user, entry in entries:
             self.rows[user_key[user]][1][option_key[option_id]] = AVAILABILTY[entry]
     
+        self.total_availability = [[0, 0] for _ in self.options]
+        for i in range(len(self.users)):
+            for j in range(len(self.options)):
+                if self.rows[i][1][j] == AVAILABLE:
+                    self.total_availability[j][0] += 1
+                    print(i, j)
+                elif self.rows[i][1][j] == NOT_PREFERED:
+                    self.total_availability[j][1] += 1
+
     def diffrent_than_last(self, i):
         return (i == 0) or (self.options[i-1].year_and_month() != self.options[i].year_and_month())
 
