c99250383f7ac5edb1db713c3db629c9b95aa23a
5K1PY <daniel@honza.info>
2022-10-11 21:25:57 +0200

    Added interval mode

--- static/js/poll.js
+++ static/js/poll.js
@@ -1,8 +1,25 @@
 $(function() {
     var $this = $(this);
+    var options = [];
+    for (var i=0;; i++) {
+        var option = $this.find(`#options-${i}`);
+        if (option.val() === undefined) {
+            break;
+        }
+        options.push(option.val());
+    }
+    console.log(options);
 
     $this.find('.option-select').on('change', function() {
-        console.log("a");
+        var i = parseInt($(this).attr('id').match(/^options-(\d)/)[1]);
+        if ($this.find('#interval-mode').is(":checked")) {
+            console.log(options);
+            for (var j=i+1; j < options.length && options[i] === options[j]; j++) {
+                console.log(j);
+                $this.find(`#options-${j}`).val(this.value).change();
+            }
+        }
+        options[i] = this.value;
     });
 
 
