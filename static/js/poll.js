3e512b1a3661c17e793b63fa0141a329da88143c
5K1PY <daniel@honza.info>
2022-10-15 12:33:38 +0200

    Work on settings - interval mode

--- static/js/poll.js
+++ static/js/poll.js
@@ -10,15 +10,12 @@ $(function() {
         }
         options.push(option.val());
     }
-    console.log(options);
 
     // interval mode
     $this.find('.option-select').on('change', function() {
         var i = parseInt($(this).attr('id').match(/^options-(\d+)$/)[1]);
         if ($this.find('#interval-mode').is(":checked")) {
-            console.log(options);
             for (var j=i+1; j < options.length && options[i] === options[j]; j++) {
-                console.log(j);
                 $this.find(`#options-${j}`).val(this.value).change();
             }
         }
@@ -28,4 +25,12 @@ $(function() {
     // enable tooltips
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+    // add shortcuts
+    $this.keypress(function(e) {
+        var key = String.fromCharCode(e.keyCode || e.which);
+        if (key == 'i') {
+            console.log($this.find('#interval-mode').click())
+        }
+    });
 });
