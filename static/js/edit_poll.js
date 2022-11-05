0461111686f9ccc5868b381e5618ad5e66be0afb
Daniel Skýpala <daniel@honza.info>
2022-11-05 22:24:44 +0100

    Added deleting options

--- static/js/edit_poll.js
+++ static/js/edit_poll.js
@@ -1,35 +1,53 @@
 $(function() {
     var $this = $(this);
 
+    function rename(el, from, to) {
+        console.log(el);
+        if ($(el).attr('id')) {
+            var id = $(el).attr('id').replace('-' + from, '-' + to);
+            $(el).attr('id', id);
+        }
+        if ($(el).attr('name')) {
+            $(el).attr('name', id);
+        }
+        if ($(el).attr('placeholder')) {
+            var placeholder = $(el).attr('placeholder').replace(from+1, to+1);
+            $(el).attr('placeholder', placeholder)
+        }
+        if ($(el).attr('data-target')) {
+            var data_target = $(el).attr('data-target').replace('-' + from, '-' + to);
+            $(el).attr('data-target', data_target)
+        }
+        if ($(el).attr('class')) {
+            var class_ = $(el).attr('class').replace('s-' + from, 's-' + to);
+            $(el).attr('class', class_);
+        }
+    }
+
+    function rename_recursive(el, from, to) {
+        rename(el, from, to)
+        // Rename ids and names
+        $(el).find('*').each(function () {
+            rename(this, from, to);
+        });
+    }
+
+    function delete_enabler() {
+        if ($this.find('.options-entry').length <= 1) {
+            $this.find('.delete-option').prop('disabled', true);
+        } else {
+            $this.find('.delete-option').prop('disabled', false);
+        }
+    }
+
     // Add row
     $this.find('#add-row').click(function() {
         var target = $($(this).data('target'))
         var old_entry = target.find('.options-entry:last');
         var new_entry = old_entry.clone(true, true);
         var elem_id = $(new_entry).attr('id');
-        var elem_num = parseInt(elem_id.replace(/options-(\d{1,4})/m, '$1')) + 1;
-        new_entry.attr('id', `options-${elem_num}`);
-    
-        // Rename ids and names
-        new_entry.find('input, div, button').each(function() {
-            console.log(this);
-            if ($(this).attr('id')) {
-                var id = $(this).attr('id').replace('-' + (elem_num - 1), '-' + (elem_num));
-                $(this).attr('name', id).attr('id', id)
-            }
-            if ($(this).attr('placeholder')) {
-                var placeholder = $(this).attr('placeholder').replace(elem_num - 1, elem_num);
-                $(this).attr('placeholder', placeholder)
-            }
-            if ($(this).attr('data-target')) {
-                var data_target = $(this).attr('data-target').replace('-' + (elem_num - 1), '-' + (elem_num));
-                $(this).attr('data-target', data_target)
-            }
-            if (this.tagName != "DIV") {
-                var class_ = $(this).attr('class').replace('-' + (elem_num - 1), '-' + (elem_num));
-                $(this).attr('class', class_);
-            }
-        });
+        var elem_num = parseInt(elem_id.replace('option-', ''));
+        rename_recursive(new_entry, elem_num, elem_num+1);
 
         // Clear values
         new_entry.find('input').each(function() {
@@ -38,15 +56,23 @@ $(function() {
 
         new_entry.find()
         old_entry.after(new_entry);
+        delete_enabler();
     });
 
     // Remove row
-    $this.find('#remove-row').click(function() {
-        if ($this.find('.options-entry').length > 1) {
-            var target = $($(this).data('target'))
-            var lastRow = target.find('.options-entry:last');
-            lastRow.remove();
+    $this.find('.delete-option').click(function() {
+        if ($this.find('.options-entry').length <= 1) {
+            return;
         }
-        remove_row_enabler();
+        var rem_num = parseInt($(this).data('target').replace('#option-', ''));
+        var target = $($(this).data('target'));
+        target.remove();
+        $this.find(".options-entry").each(function() {
+            var option_num = parseInt($(this).attr('id').replace('option-', ''));
+            if (option_num > rem_num) {
+                rename_recursive(this, option_num, option_num-1);
+            }
+        });
+        delete_enabler();
     });
 })
