7be0e7c5c4cec52626bf3c7fe0bdc4e3b09ed4a9
Daniel Skýpala <daniel@honza.info>
2022-11-09 15:42:17 +0100

    Moved option generation to poll

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datetime import date, time, timedelta
+from datetime import date, time
 from flask import Flask, render_template, redirect, request
 from werkzeug.exceptions import abort
 from urllib.parse import unquote
@@ -6,45 +6,10 @@ from constants import MODES
 
 from form import CreationForm, PollForm, EditForm
 from db import make_poll, poll_exists, read_poll, user_filled_poll, write_poll, delete_user_from_poll
+from poll import gen_new_options, gen_edit_options
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sosecret'
-
-def gen_options(data):
-    poll_options = []
-    for option_create in data:
-        day_mode = option_create['day_mode']
-        day1, day2 = option_create['day1'], option_create['day2']
-        day_increment = option_create['day_increment']
-        time_mode = option_create['time_mode']
-        time1, time2, time3 = option_create['time1'], option_create['time2'], option_create['time3']
-        time_increment = option_create['time_increment']
-
-        if day_mode == 'One day':
-            day2 = day1
-            day_increment = 1
-
-        while day1 <= day2:
-            if time_mode == "Whole day":
-                poll_options.append(day1.strftime("%Y-%m-%d"))
-            elif time_mode == "Various times":
-                for time in (time1, time2, time3):
-                    if time is not None:
-                        poll_options.append(f'{day1.strftime("%Y-%m-%d")} {time.strftime("%H:%M")}')
-            elif time_mode == "Hourly range":
-                time = time1
-                while time <= time2:
-                    poll_options.append(f'{day1.strftime("%Y-%m-%d")} {time.strftime("%H:%M")}')
-                    hours, minutes = time.hour, time.minute + time_increment
-                    hours, minutes = (hours + minutes // 60), (minutes % 60)
-                    if hours >= 24:
-                        break
-                    time = time(hours, minutes)
-            day1 = day1 + timedelta(days=day_increment)
-
-    poll_options = list(sorted(set(poll_options)))
-    return poll_options
-
 
 @app.route('/')
 def index():
@@ -56,7 +21,7 @@ def new_poll():
     form = CreationForm()
     if form.validate_on_submit():
         poll_name = form.poll_title.data  
-        poll_options = gen_options(form.options.data)
+        poll_options = gen_new_options(form.options.data)
         secret = make_poll(poll_name, poll_options)
         return redirect(f'/poll/{secret}')
 
@@ -144,8 +109,7 @@ def edit_poll(poll_id):
     errors = ""
     if request.method == "POST":
         if form.validate_on_submit():
-            for option in form.options:
-                print(option.data)
+            print(gen_edit_options(form.options.data))
         else:
             errors = form.errors
 
--- poll.py
+++ poll.py
@@ -2,7 +2,56 @@ import calendar
 from datetime import date
 import re
 
+import datetime
 from constants import AVAILABILITY, DEFAULT_AVAILABILITY
+
+def gen_new_options(data):
+    poll_options = []
+    for option_create in data:
+        day_mode = option_create['day_mode']
+        day1, day2 = option_create['day1'], option_create['day2']
+        day_increment = option_create['day_increment']
+        time_mode = option_create['time_mode']
+        time1, time2, time3 = option_create['time1'], option_create['time2'], option_create['time3']
+        time_increment = option_create['time_increment']
+
+        if day_mode == 'One day':
+            day2 = day1
+            day_increment = 1
+
+        while day1 <= day2:
+            if time_mode == "Whole day":
+                poll_options.append(day1.strftime("%Y-%m-%d"))
+            elif time_mode == "Various times":
+                for time in (time1, time2, time3):
+                    if time is not None:
+                        poll_options.append(f'{day1.strftime("%Y-%m-%d")} {time.strftime("%H:%M")}')
+            elif time_mode == "Hourly range":
+                time = time1
+                while time <= time2:
+                    poll_options.append(f'{day1.strftime("%Y-%m-%d")} {time.strftime("%H:%M")}')
+                    hours, minutes = time.hour, time.minute + time_increment
+                    hours, minutes = (hours + minutes // 60), (minutes % 60)
+                    if hours >= 24:
+                        break
+                    time = datetime.time(hours, minutes)
+            day1 = day1 + datetime.timedelta(days=day_increment)
+
+    poll_options = list(sorted(set(poll_options)))
+    return poll_options
+
+def gen_edit_options(data):
+    poll_options = []
+    for edit_option in data:
+        day = edit_option['day']
+        time = edit_option['time']
+        if time is None:
+            poll_options.append(f'{day.strftime("%Y-%m-%d")}')
+        else:
+            poll_options.append(f'{day.strftime("%Y-%m-%d")} {time.strftime("%H:%M")}')
+
+    poll_options = list(sorted(set(poll_options)))
+    return poll_options
 
 class Option:
     def __init__(self, option_id, text):
