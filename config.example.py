a8ee6f04460cf0d6163df563cf27dc65368b7180
Daniel Skýpala <daniel@honza.info>
2022-12-10 22:20:08 +0100

    Added example config

---
+++ config.example.py
@@ -0,0 +1,11 @@
+# Example config
+
+# Secret key for flask. Generate with:
+# import secrets; print(secrets.token_hex())
+SECRET_KEY = "340e89115efb899c9b18cf828e118f9733c91bde35a157377a2dca99a97e296e"
+
+# Database access info
+HOST = "localhost" # or leave empty to use UNIX socket
+DATABASE = "postgres"
+USER = "user"
+PASSWORD = "password"
