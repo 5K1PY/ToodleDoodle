c7b32993209731471fe0776e2d94b975fe4a2c01
Daniel Skýpala <daniel@honza.info>
2022-12-10 22:19:13 +0100

    Documntation for connecting to UNIX socket

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import secrets
 with open("config.py", "w") as f:
     f.write(f'SECRET_KEY = "{secrets.token_hex()}"\n\n')
     print("DATABASE INFO:")
-    f.write(f'HOST = "{input("HOST: ")}"\n')
+    f.write(f'HOST = "{input("HOST (leave empty for UNIX socket): ")}"\n')
     f.write(f'DATABASE = "{input("DATABASE: ")}"\n')
     f.write(f'USER = "{input("USER: ")}"\n')
     f.write(f'PASSWORD = "{input("PASSWORD: ")}"\n')
