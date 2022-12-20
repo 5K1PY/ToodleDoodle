ac51dfe94feca59e5711ea89e3864f7d5f9a726b
Daniel Skýpala <daniel@honza.info>
2022-12-20 13:33:26 +0100

    Changed no-js working to noscript

--- static/js/base.js
+++ static/js/base.js
@@ -1,8 +1,6 @@
 $(function() {
     var $this = $(this);
 
-    $this.find("#no-js").hide();
-
     // enable tooltips
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
